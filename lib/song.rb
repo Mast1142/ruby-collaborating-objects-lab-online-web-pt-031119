ea6bd1ff7b816b4c7965e74a774030c2daf916c3
Samuel T <ssamt1142@gmail.com>
2019-04-12 02:59:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,12 +20,19 @@ attr_reader :artist
   def self.new_by_filename(file)
     binding.pry
     nomp3 = file.gsub(".mp3","")
+    binding.pry
     artist_info = nomp3.split(" - ")
+    binding.pry
     song_name = artist_info[1]
+    binding.pry
     song_artist = artist_info[0]
+    binding.pry
     song_genre = artist_info[2]
+    binding.pry
     song = self.new(song_name)
+    binding.pry
     song.name = song_name
+    binding.pry
     song.artist = song_artist
   end
 
