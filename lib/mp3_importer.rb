dcf39eaa612d02a00a6de8c5a6902b0a7072a070
Samuel T <ssamt1142@gmail.com>
2019-04-12 03:57:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -8,7 +8,7 @@ attr_accessor :path
    Dir.entries(path).select {|f| !File.directory? f}
  end
 
- def import(list_of_filenames)
+ def import
    list_of_filenames.each {|file| Song.new_by_filename(file)}
 
  end
